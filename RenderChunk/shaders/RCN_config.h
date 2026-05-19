afee116c28351765f80d39a1ced6f0f57b1cc83b
Jumao15071 <603070637@qq.com>
2026-05-19 23:45:45 +0800

    1

--- RenderChunk/shaders/RCN_config.h
+++ RenderChunk/shaders/RCN_config.h
@@ -1,13 +1,4 @@
 //这一坨是为了减小编译后的体积
-// 0: 有夜视 + 有亮度
-// 1: 有夜视 + 无亮度
-// 2: 无夜视 + 有亮度
-// 3: 无夜视 + 无亮度
-#define SUBPACKS 0
-#if SUBPACKS == 0 || SUBPACKS == 1
-    #define NIGHT_VISION
-#endif
-
 #if !defined(DEPTH_ONLY) && !defined(DEPTH_ONLY_OPAQUE)
     #if !defined(INSTANCING) && !defined(INSTANCING__ON) && !defined(RENDER_AS_BILLBOARDS)
         
@@ -37,8 +28,26 @@
     #endif 
 #endif
 
-#if SUBPACKS == 1 || SUBPACKS == 3
+// 0: 有夜视 + 有亮度 + 有区块显示
+// 1: 有夜视 + 无亮度 + 有区块显示
+// 2: 无夜视 + 有亮度 + 有区块显示
+// 3: 无夜视 + 无亮度 + 有区块显示
+// 4: 有夜视 + 有亮度 + 无区块显示
+// 5: 有夜视 + 无亮度 + 无区块显示
+// 6: 无夜视 + 有亮度 + 无区块显示
+// 7: 无夜视 + 无亮度 + 无区块显示
+#define SUBPACKS 4
+
+#if SUBPACKS == 0 || SUBPACKS == 1 || SUBPACKS == 4 || SUBPACKS == 5
+    #define NIGHT_VISION
+#endif
+#if SUBPACKS == 1 || SUBPACKS == 3 || SUBPACKS == 5 || SUBPACKS == 7
     #ifdef LIGHT_OVERLAY
         #undef LIGHT_OVERLAY
+    #endif
+#endif
+#if SUBPACKS == 4 || SUBPACKS == 5 || SUBPACKS == 6 || SUBPACKS == 7
+    #ifdef CHUNK_BORDERS
+        #undef CHUNK_BORDERS
     #endif
 #endif
